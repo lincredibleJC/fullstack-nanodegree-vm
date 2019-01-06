6fd744a7b6dc01611f295c4156744eb3827a8da5
lincredibleJC <lin_jiachao@hotmail.com>
2019-01-06 13:33:21 +0800

    Change the spam posts into cheese

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -10,6 +10,7 @@ def get_posts():
   """Return all posts from the 'database', most recent first."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
+  c.execute("UPDATE posts SET content= 'cheese' WHERE content LIKE '%spam%'")
   c.execute("SELECT content, time FROM posts ORDER BY time DESC")
   posts = ([str(bleach.clean(row[0])), row[1]] for row in c.fetchall()) # c.fetchall() returns a table [[content, time],...]
   db.close()
@@ -24,3 +25,4 @@ def add_post(content):
   c.execute("INSERT into posts VALUES (%s)", (content,))
   db.commit()
   db.close()
+  
