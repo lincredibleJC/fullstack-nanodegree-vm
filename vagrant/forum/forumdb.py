5e6e773f09198368542cb383fe4bceaf58e02e5b
lincredibleJC <lin_jiachao@hotmail.com>
2019-01-06 13:26:44 +0800

    Sanitise input and existing content with bleach

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,6 +2,8 @@
 
 import psycopg2
 
+import bleach
+
 DBNAME = "forum"
 
 def get_posts():
@@ -9,7 +11,7 @@ def get_posts():
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
   c.execute("SELECT content, time FROM posts ORDER BY time DESC")
-  posts = c.fetchall()
+  posts = ([str(bleach.clean(row[0])), row[1]] for row in c.fetchall()) # c.fetchall() returns a table [[content, time],...]
   db.close()
   return posts
   
@@ -18,6 +20,7 @@ def add_post(content):
   """Add a post to the 'database' with the current timestamp."""
   db = psycopg2.connect(database=DBNAME)
   c = db.cursor()
+  content = bleach.clean(content)
   c.execute("INSERT into posts VALUES (%s)", (content,))
   db.commit()
   db.close()
